36c97741c9fcd0d21eee82efa22f3b8344a2cc3d
Sha Quan <940692313@qq.com>
2024-01-07 20:42:38 +0800

    Signed-off-by: Sha Quan <940692313@qq.com>

---
+++ next-live-card/pages/index/index.vue
@@ -0,0 +1,42 @@
+<template>
+	<view class="page">
+		<view class="card">
+			
+			<view class="logo-wapper">
+				<image src="../../static/logo-1.jpg" mode="widthFix" class="logo"></image>
+			</view>
+			
+			<view class="words-wapper">
+				<view class="words" style="font-weight: bold;">
+					static學院
+				</view>
+				<view class="words">
+					專注高質量IT課程
+				</view>
+				<view class="words" style="margin-top: 30upx;">
+					940692313@qq.com
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css"); 
+</style>
